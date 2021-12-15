9f533d7e84e9329eb15f25b3acccbd44f71aa6f1
Christian Albert <calbert1209@gmail.com>
2021-12-15 22:27:55 +0900

    format project

--- src/smoke/__tests__/smoke.test.ts
+++ src/smoke/__tests__/smoke.test.ts
@@ -1,7 +1,7 @@
 import { multiply } from "../smoke";
 
-describe("multiply", () =>{
-  test("should multiply two positive numbers", () =>{
-    expect(multiply(6,7)).toEqual(42);
+describe("multiply", () => {
+  test("should multiply two positive numbers", () => {
+    expect(multiply(6, 7)).toEqual(42);
   });
 });
